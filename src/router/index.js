79e30f99554dcea21fb39f199cd69c3230965ea2
Brent <brentmaggard@gmail.com>
2018-07-16 22:13:07 -0400

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,11 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '',
+      name: 'HomeMain',
+      component: Home
+    },
+    {
       path: '/',
       name: 'Home',
       component: Home
